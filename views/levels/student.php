dcd5283e7ca5d63b184b479fd11a9e90af18a8c1
HeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2024-11-26 11:17:59 +0100

    Fix student bepaling

--- views/levels/student.php
+++ views/levels/student.php
@@ -35,6 +35,42 @@
             ?>
         </div>
        
+        <h1 class="m-2 text-2xl leading-none text-gray-900 w-fit">Results</h1>
+        <table class="table-auto w-full border-collapse border border-gray-400 mb-4 mt-4">
+            <thead>
+                <tr>
+                    <th class="border bg-gray-300 border-gray-300 px-4 py-2">Your results</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="h-64">
+                    <td class="border border-gray-300 px-4 py-2">
+                    <textarea class="h-64 w-full" id="meow" name="meow" style="resize: vertical; overflow: auto;">I am level 3 because I am very good at stuff</textarea>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="flex flex-row float-right mb-4 items-center">
+            <p class="mr-8">upload files:</p>
+            <form class="flex flex-row items-center" action="file-upload.php" method="post" enctype="multipart/form-data">
+            <input name="userfile[]" type="file" />
+            <input class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 border-4 text-lg text-white py-1 px-6 rounded" type="submit" value="Save" />
+            </form>
+        </div>
+        <table class="table-auto w-full border-collapse border border-gray-400 mb-4 mt-4">
+            <thead>
+                <tr>
+                    <th class="border bg-gray-300 border-gray-300 px-4 py-2">Reply</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="h-64">
+                    <td class="border border-gray-300 px-4 py-2">
+                    <textarea class="h-64 w-full" id="meow" name="meow" style="resize: vertical; overflow: auto;">You are indeed very good at stuff</textarea>
+                </td>
+                </tr>
+            </tbody>
+        </table>
         <script>
             function openTab(evt, tabName) {
                 var i, tabcontent, tablinks;
